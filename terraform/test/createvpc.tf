17581a8b77a8fa88314d9e64f4a04583fd8bf7c2
Weerawud Wongpornpaisarn <weerawud@Weerawuds-MacBook-Pro.local>
2019-01-21 16:41:17 +0700

    commit

---
+++ terraform/test/createvpc.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region ="us-east-1"
+  profile = "yayee-free"
+}
+resource "aws_instance" "example" {
+  #ami = "ami-035be7bafff33b6b6"
+  ami = "ami-0ac019f4fcb7cb7e6"
+  instance_type = "t2.micro"
+}
